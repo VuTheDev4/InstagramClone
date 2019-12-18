806273e5f840f286a60f17abb2a9596a41efe599
Vu Duong <vduong4@gmail.com>
2019-12-17 23:26:19 -0500

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Vu Duong on 12/17/19.
+//  Copyright © 2019 Vu Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
