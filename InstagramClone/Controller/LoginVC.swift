9fc66cdbf98677ec3bec811dbc949b1817a9bf68
Vu Duong <vduong4@gmail.com>
2019-12-17 23:30:07 -0500

    First Commit

---
+++ InstagramClone/Controller/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  InstagramClone
+//
+//  Created by Vu Duong on 12/17/19.
+//  Copyright © 2019 Vu Duong. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
